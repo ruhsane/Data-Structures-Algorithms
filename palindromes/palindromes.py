bbb9f2c00e3fa37bcefb2330e216370592e1e9b2
Rushani Shawuti <ruhsane924@gmail.com>
2019-04-09 23:38:39 -0700

    added comments

--- palindromes/palindromes.py
+++ palindromes/palindromes.py
@@ -14,6 +14,7 @@ def is_palindrome(text):
     # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), 'input is not a string: {}'.format(text)
 
+    # lower case the input, and ingore anything other than lower letters
     filteredText = ''.join(c for c in text.lower() if c in string.ascii_lowercase)
 
     # return is_palindrome_iterative(filteredText)
@@ -29,11 +30,16 @@ def is_palindrome_iterative(text):
     left = 0
     right = len(text) - 1
 
+    # run loop until leftMost index and rightMost index cross over
     while left <= right:
+        # if the leftMost value and rightMost value in current iteration are not the same, return false
         if text[left] != text[right]:
             return False
+        # move left and right towards middle for next iteration
         left += 1
         right -= 1
+
+    # after the loop is done when indexes cross and didn't return false -- return true
     return True
 
 
@@ -44,15 +50,22 @@ def is_palindrome_recursive(text, left=None, right=None):
 
     # this only runs first iteration
     if left == None and right == None:
+        # begin with left being the first index, and left being the last index
         left = 0
         right = len(text) - 1
     
+    # recursively call function only if the left index and right index doesnt cross over
     if left <= right:
+        # if the left value and right value are not the same, return false
         if text[left] != text[right]:
             return False
+         # move left and right towards middle for next iteration of call
         left += 1
         right -= 1
+        # call the function again to check palindrome with updated left and right index
         return is_palindrome_recursive(text, left, right)
+
+    # returns true until left index and right index crossed over and didnt return false during the process
     return True
 
 def main():
