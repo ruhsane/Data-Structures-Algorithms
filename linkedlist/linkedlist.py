55971afd99640908fd2006c5902a4494a488f5e1
Rushani Shawuti <ruhsane924@gmail.com>
2019-04-15 22:23:39 -0700

    replace method done and passes test

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -180,7 +180,17 @@ class LinkedList(object):
         Worst case running time: ??? under what conditions? [TODO]"""
         # TODO: Find the node containing the given old_item and replace its
         # data with new_item, without creating a new node object
-        pass
+        
+        node = self.head
+
+        while node is not None:
+            if node.data == old_item:
+                node.data = new_item
+                return 
+            else:
+                node = node.next
+        
+        raise ValueError('Item not found: {}'.format(old_item))
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
