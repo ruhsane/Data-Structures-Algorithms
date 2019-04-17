28e644b3b9429ef8b05df82490855c1515a63f35
Rushani Shawuti <ruhsane924@gmail.com>
2019-04-16 23:50:43 -0700

    isEmpty and length method implemented

--- queue.py
+++ queue.py
@@ -21,11 +21,13 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
-
+        # Check if empty
+        return self.list.is_empty()
+        
     def length(self):
         """Return the number of items in this queue."""
         # TODO: Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
