33715db4ad956bb99f085dfcc874e3af25990909
Rushani Shawuti <ruhsane924@gmail.com>
2019-04-16 21:40:30 -0700

    implement is_empty

--- stack.py
+++ stack.py
@@ -22,6 +22,11 @@ class LinkedStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
+        
+        if self.length == 0:
+            return True
+        else:
+            return False
 
     def length(self):
         """Return the number of items in this stack."""
