39e0034da44f8be2cc1726524694c7252ef2c7de
Rushani Shawuti <ruhsane924@gmail.com>
2019-04-22 12:07:18 -0700

    load factor method implemented

--- hashtable.py
+++ hashtable.py
@@ -27,7 +27,7 @@ class HashTable(object):
         """Return the load factor, the ratio of number of entries to buckets.
         Best and worst case running time: ??? under what conditions? [TODO]"""
         # TODO: Calculate load factor
-        # return ...
+        return self.length() / self.size
 
     def keys(self):
         """Return a list of all keys in this hash table.
