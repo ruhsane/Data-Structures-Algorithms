70f23485687ccb449c9db19edd8135197c019e37
Rushani Shawuti <ruhsane924@gmail.com>
2019-04-29 13:52:26 -0700

    problem

---
+++ redact_problem.py
@@ -0,0 +1,5 @@
+''' 
+Problem: A web application needs to redact some words. Write a function that takes 2 arrays of words (strings),
+and return a new array of words in the first array (the text) taht are not in the second array (the redacted words) 
+'''
+
