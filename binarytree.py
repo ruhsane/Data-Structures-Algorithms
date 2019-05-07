e432dab59b885b5fc0b0398626c78420667a4188
Rushani Shawuti <ruhsane924@gmail.com>
2019-05-07 11:28:13 -0700

    search method implemented and passes test

--- binarytree.py
+++ binarytree.py
@@ -100,7 +100,7 @@ class BinarySearchTree(object):
         # Find a node with the given item, if any
         node = self._find_node_recursive(item, self.root)
         # TODO: Return the node's data if found, or None
-        return node.data if ... else None
+        return node.data if node else None
 
     def insert(self, item):
         """Insert the given item in order into this binary search tree.
