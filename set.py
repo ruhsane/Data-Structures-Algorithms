6ca7610a93b0fed80e7d9452475cb476173e743c
Rushani Shawuti <ruhsane924@gmail.com>
2019-05-16 21:42:53 -0700

    union implemented and passes test

--- set.py
+++ set.py
@@ -5,18 +5,51 @@ from hashtable import HashTable
 class Set(object):
 
     def __init__(self, elements=None):
-        self.set = HashTable()
+        self.items = HashTable()
         self.size = 0
 
         if elements is not None:
             for element in elements:
-                for key, value in element:
-                    self.add(key, value)
+                self.add(element)
+
+    def __iter__(self):
+        for el in self.all_items():
+            yield el
+
+    def all_items(self):
+        return self.items.keys()
 
     def contains(self, key):
         '''return a boolean indicating whether element is in this set'''
-        return self.set.contains(key)
+        return self.items.contains(key)
 
-    def add(self, key, value):
+    def length(self):
+        return self.size
+
+    def add(self, element):
         '''add element to this set, if not present already'''
-        return self.set.set(key, value)
+        self.size += 1
+        return self.items.set(element, element)
+
+    def remove(self, element):
+        ''' remove element from this set, if present, or else raise KeyError '''
+        self.size -= 1
+        return self.items.delete(element)
+
+    def union(self, other_set):
+        ''' return a new set that is the union of this set and other_set '''
+        # initialize union with other_set
+        union = other_set
+        for item in self:
+            # add every item in our set to union
+            union.add(item)
+        return union
+
+    def intersection(self, other_set):
+        ''' return a new set that is the intersection of this set and other_set '''
+
+    def difference(self, other_set):
+        ''' return a new set that is the difference of this set and other_set '''
+
+    def is_subset(self, other_set):
+        ''' return a boolean indicating whether other_set is a subset of this set '''
